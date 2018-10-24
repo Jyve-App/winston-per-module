728b71cefb87521aa1e78d7aede78db5495c342f
Tyler Hall <tyler@jyve.io>
2018-10-24 10:36:31 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import winston from 'winston'
+import colors from 'colors/safe'
+
+const _colors = ['red', 'yellow', 'green', 'blue', 'magenta', 'white']
+
+const { format } = winston
+
+function buildFormats (label, formats) {
+  formats = formats || []
+  const _formats = [
+    format.label({ label, message: true }),
+    format.cli(),
+    format.timestamp(),
+    format.simple(),
+    ...formats]
+  return format.combine(..._formats)
+}
+
+let i = 0
+module.exports = (module, level) => {
+  level = level || 'debug'
+
+  module = colors[_colors[i]](module)
+  let next = (i + 1) % _colors.length
+  i = next
+
+  const transports = [
+    new winston.transports.Console({
+      level,
+      timestamp: () => (new Date()).toISOString()
+    })
+  ]
+
+  return (label, loggerOptions) => {
+    label = label ? `${module}:${colors[_colors[next]](label)}` : module
+    loggerOptions = loggerOptions || {}
+
+    if (winston.loggers.has(label)) {
+      return winston.loggers.get(label)
+    }
+
+    let defaultOptions = {
+      format: buildFormats(label),
+      transports
+    }
+
+    let options = {
+      ...defaultOptions,
+      ...loggerOptions
+    }
+
+    return winston.loggers.add(label, options)
+  }
+}
